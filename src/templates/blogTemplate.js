ecbb64c7ba48e7bdd6462c0d19f565aaa447939d
Juls3 <ju.sauvan@gmail.com>
2020-03-08 18:51:43 +0100

    changed blog template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,16 +9,29 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+      <Layout>
+          <Helmet>
+              <title>Julien Sauvan - Blog</title>
+              <meta name="description" content="Julien Sauvan, french data scientist" />
+          </Helmet>
+          <div id="main" className="alt">
+          <section id="one">
+              <div className="inner">
+
+        <div className="blog-post-container">
+          <div className="blog-post">
+            <h1>{frontmatter.title}</h1>
+            <h4>{frontmatter.date}</h4>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+            </div>
+        </div>
+        </div>
+        </section>
     </div>
+    </Layout>
   )
 }
 
