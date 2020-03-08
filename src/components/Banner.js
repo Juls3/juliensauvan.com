f4d7dae94927aba5c8810d7201eddd6fb74fdc0e
Juls3 <ju.sauvan@gmail.com>
2020-03-08 12:29:08 +0100

    minor changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,6 +6,23 @@ const Banner = props => (
       <header className="major">
         <h1>Hi, my name is Julien</h1>
       </header>
+       <ul className="icons">
+        <li>
+          <a href="https://twitter.com/JulienSauvan" aria-label="Twitter" className="icon alt fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Juls3" aria-label="GitHub" className="icon alt fa-github">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/julien-sauvan-b5a40982/" aria-label="LinkedIn" className="icon alt fa-linkedin">
+            <span className="label">LinkedIn</span>
+          </a>
+        </li>
+      </ul>
       <div className="content">
         <p>
           I'm a french Data Scientist working in Paris.
@@ -19,6 +36,7 @@ const Banner = props => (
   <br />
 Member of the <a href="https://www.capgemini.com/fr-fr/cas-client/ia-traumatrix/">TrauMatrix project</a>, a multi-disciplinary ambitious project gathering traumatologists, researchers, consultants and data scientists to build a decision support tool to better handle trauma patients with traumatic brain injuries and hemorrhagic shock.
         </p>
+
         <ul className="actions">
           <li>
             <a href="/blog" name="Check my blog" className="button next scrolly">
--- src/pages/index.js
+++ src/pages/index.js
@@ -34,42 +34,7 @@ class HomeIndex extends React.Component {
 
         <div id="main">
           <section id="two">
-            <div className="inner">
-              <header className="major">
-                <h2>Welcome on my Internet space</h2>
-              </header>
-              <ul className="icons">
-        <li>
-          <a href="https://twitter.com/JulienSauvan" aria-label="Twitter" className="icon alt fa-twitter">
-            <span className="label">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Juls3" aria-label="GitHub" className="icon alt fa-github">
-            <span className="label">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/julien-sauvan-b5a40982/" aria-label="LinkedIn" className="icon alt fa-linkedin">
-            <span className="label">LinkedIn</span>
-          </a>
-        </li>
-      </ul>
-              <p>
-               You'll find articles about data science on my blog abd my data science portfolio. I'm also a photographer on my spare time. Feel free to reach me !
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link
-                    aria-label="Link to Landing Page"
-                    to="/projects"
-                    className="button next"
-                  >
-                    Check my projects
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            
           </section>
           <section id="one" className="tiles">
             <article style={{ backgroundImage: `url(${pic01})` }}>
